6add022ac2745fa5bdabd7a85ec22c7d5fc69d15
Alex Paton <G00424185@atu.ie>
2024-10-23 14:47:33 +0100

    Started Project and added Post and Get Requests

---
+++ src/main/java/ie/atu/week6cicd/ProductController.java
@@ -0,0 +1,34 @@
+package ie.atu.week6cicd;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductController {
+    private ProductService myProduct;
+
+    public ProductController(ProductService myProduct){
+        this.myProduct = myProduct;
+    }
+
+    @PostMapping("/newProduct")
+    public List<Product> newProduct(@RequestBody Product product)
+
+    {
+        //business logic to add this to a database
+        //return list of all products from the database
+        return myProduct.addProduct(product);
+
+    }
+    @GetMapping("/getProducts")
+    public List<Product> allProducts()
+    {
+        return myProduct.getProducts();
+    }
+}
