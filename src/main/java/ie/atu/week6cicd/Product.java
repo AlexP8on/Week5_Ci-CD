4f470d2aedd155a536e7ad29e8036dce32063154
Alex Paton <G00424185@atu.ie>
2024-10-23 15:21:55 +0100

    Added Error messages

--- src/main/java/ie/atu/week6cicd/Product.java
+++ src/main/java/ie/atu/week6cicd/Product.java
@@ -1,5 +1,8 @@
 package ie.atu.week6cicd;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +11,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+
+    @Min(value= 0,message="You must enter a valid id")
     private int id;
-
+    @NotBlank(message="You must enter a name")
     private String name;
-
+   @PositiveOrZero(message="You must enter a valid price")
     private double price;
 }
